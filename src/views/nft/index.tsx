a11cee698a59422454b37d029ea9224a50a60935
gsheen <gsheen@neurable.com>
2022-07-22 10:29:02 -0700

    dependencies

--- src/views/nft/index.tsx
+++ src/views/nft/index.tsx
@@ -5,7 +5,10 @@ import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 export const NFTView = () => {
   const wallet = useWallet();
+  // const [file, setFile] = useState<MetaplexFile>();
+  const [nftUri, setNftUri] = useState('');
   const { connection } = useConnection();
+
   const metaplex = Metaplex.make(connection)
                            .use(walletAdapterIdentity(wallet))
                            .use(bundlrStorage());
@@ -17,7 +20,7 @@ export const NFTView = () => {
     timeout: 60000,
   }));
   
-  async function execute(e) {
+  async function upload(e) {
     const browserFile: File = e.target.files[0];
     const file: MetaplexFile = await toMetaplexFileFromBrowser(browserFile);
     // const price = await metaplex.storage().getUploadPriceForFile(file);
@@ -26,37 +29,26 @@ export const NFTView = () => {
     const { uri, metadata } = await metaplex
       .nfts()
       .uploadMetadata({
-          name: "My NFT",
           image: file,
       })
       .run();
+    setNftUri(uri);
     console.log(`Your picture has been uploaded to Arweave at:\n${metadata.image}`)
+  }
 
+  async function mint() {
     const { nft } = await metaplex
       .nfts()
       .create({
-          uri: uri,
+          uri: nftUri,
           name: "My NFT",
           sellerFeeBasisPoints: 500, // Represents 5.00%.
       })
       .run(); 
 
     const address = nft.mintAddress.toString()
-    console.log(`You can view your NFT on the Solana Explorer at:\nhttps://explorer.solana.com/address/${address}?cluster=devnet`)
-
+    console.log(`You can view your minted NFT on the Solana Explorer at:\nhttps://explorer.solana.com/address/${address}?cluster=devnet`)
   }
-
-  // async function onSelect() {
-
-  // }
-
-  // async function onUpload() {
-
-  // }
-
-  // async function onMint() {
-
-  // }
 
   return (
     <div className="md:hero mx-auto p-4">
@@ -68,42 +60,17 @@ export const NFTView = () => {
           <p>Upload any photo and mint it as an NFT on Solana!</p>
         </h4>
         <div className="text-center">
-          <input type='file' onChange={execute}/>
+          <input type='file' onChange={upload}/>
         </div>
+        <button
+          className="group w-60 m-2 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+          onClick={mint} disabled={!nftUri}
+        >
+          <span className="block group-disabled:hidden" > 
+              Mint NFT 
+          </span>
+        </button>
       </div>
     </div>
   );
-  // return (
-  //   <div className="md:hero mx-auto p-4">
-  //     <div className="md:hero-content flex flex-col">
-  //       <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-  //         Mint an NFT
-  //       </h1>
-  //       <h4 className="md:w-full text-center text-slate-300 my-2">
-  //         <p>Upload any photo and mint it as an NFT on Solana!</p>
-  //       </h4>
-  //       <div className="text-center">
-  //         <input type='file' onChange={execute}/>
-  //       </div>
-  //       <button
-  //         className="group w-60 m-2 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-  //         onClick={onUpload} disabled={!browserFile}
-  //       >
-  //         <span className="block group-disabled:hidden" > 
-  //             Upload Photo 
-  //         </span>
-  //       </button>
-  //       <button
-  //         className="group w-60 m-2 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-  //         onClick={onMint} disabled={!uri}
-  //       >
-  //         <span className="block group-disabled:hidden" > 
-  //             Mint NFT 
-  //         </span>
-  //       </button>
-  //     </div>
-  //   </div>
-  // );
-
-
 };
