2b35fa40ad213f2fa81222c1424cadddc0e6585f
gsheen <gsheen@neurable.com>
2022-07-27 09:18:56 -0700

    site visit demo

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,18 +1,17 @@
-// Next, React
-import { FC, useEffect, useState } from 'react';
-import Link from 'next/link';
-
-// Wallet
+import { Metaplex, MetaplexFile, bundlrStorage, BundlrStorageDriver, toMetaplexFileFromBrowser, walletAdapterIdentity } from "@metaplex-foundation/js";
+import { useState, useEffect } from "react";
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
-
-// Components
 import { RequestAirdrop } from '../../components/RequestAirdrop';
-import pkg from '../../../package.json';
-
-// Store
 import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
 
-export const HomeView: FC = ({ }) => {
+export const HomeView = () => {
+  const [nftUri, setNftUri] = useState('');
+  const [arweave, setArweave] = useState('');
+  const [name, setName] = useState('');
+  const [symbol, setSymbol] = useState('');
+  const [nftLink, setNftLink] = useState('');
+
+
   const wallet = useWallet();
   const { connection } = useConnection();
 
@@ -26,27 +25,109 @@ export const HomeView: FC = ({ }) => {
     }
   }, [wallet.publicKey, connection, getUserSOLBalance])
 
-  return (
+  const metaplex = Metaplex.make(connection)
+                           .use(walletAdapterIdentity(wallet))
+                           .use(bundlrStorage());
+  // const storage = metaplex.storage().driver() as BundlrStorageDriver;
+  
+  metaplex.use(bundlrStorage({
+    address: 'https://devnet.bundlr.network',
+    providerUrl: 'https://api.devnet.solana.com',
+    timeout: 60000,
+  }));
+  
+  async function upload(e) {
+    const browserFile: File = e.target.files[0];
+    const file: MetaplexFile = await toMetaplexFileFromBrowser(browserFile);
+    // const price = await metaplex.storage().getUploadPriceForFile(file);
+    // await storage.fund(price);
 
+    const { uri, metadata } = await metaplex
+      .nfts()
+      .uploadMetadata({
+          image: file,
+      })
+      .run();
+    setNftUri(uri);
+    setArweave(metadata.image)
+  }
+
+  const showArweave = () => {
+    return (
+      <div className='text-center text-xl mt-8 mb-8'>
+          <h2>
+            Your photo has been uploaded to Arweave <b><a href={arweave} target='_blank'>here</a></b>
+          </h2>
+      </div> 
+    )
+  }
+
+  const showNftLink = () => {
+    return (
+      <div className='text-center text-xl mt-8 mb-8'>
+        <h2>
+          View your minted NFT on the Solana Explorer <b><a href={nftLink} target='_blank'>here</a></b>
+        </h2>
+      </div>
+    )
+  }
+
+  const mintNFT = () => {
+    return (
+      <div className="text-center container mt-5">
+        <form>
+          <label> Name: </label>
+          <input className='mr-5' type="text" required
+                    onChange={(e) => setName(e.target.value)}/>
+          <label> Symbol: </label>
+          <input type="text" required
+                    onChange={(e) => setSymbol(e.target.value)}/>
+        </form>
+        <button
+          className="group w-60 mt-8 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+          onClick={mint}
+        >
+          <span className="block group-disabled:hidden" > 
+              Mint NFT 
+          </span>
+        </button>
+      </div>
+    )
+  }
+
+  async function mint() {
+    const { nft } = await metaplex
+      .nfts()
+      .create({
+          uri: nftUri,
+          name: name,
+          sellerFeeBasisPoints: 500, // Represents 5.00%.
+          symbol: symbol
+      })
+      .run(); 
+
+    const address = nft.mintAddress.toString()
+    setNftLink(`https://explorer.solana.com/address/${address}?cluster=devnet`)
+  }
+
+
+  return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
-        <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
-          Scaffold Lite <span className='text-sm font-normal align-top text-slate-700'>v{pkg.version}</span>
+        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
+          Mint an NFT
         </h1>
-        <h4 className="md:w-full text-center text-slate-300 my-2">
-          <p>Simply the fastest way to get started.</p>
-          Next.js, tailwind, wallet, web3.js, and more.
+        <h4 className="md:w-full text-center text-slate-300 my-4 text-2xl">
+          <p>Upload any photo and mint it as an NFT on Solana!</p>
         </h4>
-        <div className="max-w-md mx-auto mockup-code bg-primary p-6 my-2">
-          <pre data-prefix=">">
-            <code className="truncate">Start building on Solana  </code>
-          </pre>
-        </div>        
-          <div className="text-center">
-          <RequestAirdrop />
-          {/* {wallet.publicKey && <p>Public Key: {wallet.publicKey.toBase58()}</p>} */}
-          {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
+        <RequestAirdrop />
+        {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
+        <div className="text-center mt-8">
+          <input type='file' onChange={upload}/>
         </div>
+        {arweave && showArweave()}
+        {nftUri && mintNFT()}
+        {nftLink && showNftLink()}
       </div>
     </div>
   );
--- src/views/nft/index.tsx
+++ src/views/nft/index.tsx
@@ -1,20 +1,34 @@
 import { Metaplex, MetaplexFile, bundlrStorage, BundlrStorageDriver, toMetaplexFileFromBrowser, walletAdapterIdentity } from "@metaplex-foundation/js";
-import { Connection, clusterApiUrl, Keypair } from "@solana/web3.js";
-import { FC, useState, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { useWallet, useConnection } from '@solana/wallet-adapter-react';
+import { RequestAirdrop } from '../../components/RequestAirdrop';
+import useUserSOLBalanceStore from '../../stores/useUserSOLBalanceStore';
 
 export const NFTView = () => {
-  const wallet = useWallet();
-  // const [file, setFile] = useState<MetaplexFile>();
   const [nftUri, setNftUri] = useState('');
-  const [arweave, setArweave] = useState('')
+  const [arweave, setArweave] = useState('');
+  const [name, setName] = useState('');
+  const [symbol, setSymbol] = useState('');
   const [nftLink, setNftLink] = useState('');
+
+
+  const wallet = useWallet();
   const { connection } = useConnection();
+
+  const balance = useUserSOLBalanceStore((s) => s.balance)
+  const { getUserSOLBalance } = useUserSOLBalanceStore()
+
+  useEffect(() => {
+    if (wallet.publicKey) {
+      console.log(wallet.publicKey.toBase58())
+      getUserSOLBalance(wallet.publicKey, connection)
+    }
+  }, [wallet.publicKey, connection, getUserSOLBalance])
 
   const metaplex = Metaplex.make(connection)
                            .use(walletAdapterIdentity(wallet))
                            .use(bundlrStorage());
-  const storage = metaplex.storage().driver() as BundlrStorageDriver;
+  // const storage = metaplex.storage().driver() as BundlrStorageDriver;
   
   metaplex.use(bundlrStorage({
     address: 'https://devnet.bundlr.network',
@@ -40,7 +54,7 @@ export const NFTView = () => {
 
   const showArweave = () => {
     return (
-      <div className='text-center'>
+      <div className='text-center text-xl mt-8 mb-8'>
           <h2>
             Your photo has been uploaded to Arweave <b><a href={arweave} target='_blank'>here</a></b>
           </h2>
@@ -50,10 +64,33 @@ export const NFTView = () => {
 
   const showNftLink = () => {
     return (
-      <div className='text-center'>
+      <div className='text-center text-xl mt-8 mb-8'>
         <h2>
           View your minted NFT on the Solana Explorer <b><a href={nftLink} target='_blank'>here</a></b>
         </h2>
+      </div>
+    )
+  }
+
+  const mintNFT = () => {
+    return (
+      <div className="text-center container mt-5">
+        <form>
+          <label> Name: </label>
+          <input className='mr-5' type="text" required
+                    onChange={(e) => setName(e.target.value)}/>
+          <label> Symbol: </label>
+          <input type="text" required
+                    onChange={(e) => setSymbol(e.target.value)}/>
+        </form>
+        <button
+          className="group w-60 mt-8 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
+          onClick={mint}
+        >
+          <span className="block group-disabled:hidden" > 
+              Mint NFT 
+          </span>
+        </button>
       </div>
     )
   }
@@ -63,8 +100,9 @@ export const NFTView = () => {
       .nfts()
       .create({
           uri: nftUri,
-          name: "My NFT",
+          name: name,
           sellerFeeBasisPoints: 500, // Represents 5.00%.
+          symbol: symbol
       })
       .run(); 
 
@@ -72,27 +110,23 @@ export const NFTView = () => {
     setNftLink(`https://explorer.solana.com/address/${address}?cluster=devnet`)
   }
 
+
   return (
     <div className="md:hero mx-auto p-4">
       <div className="md:hero-content flex flex-col">
         <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-tr from-[#9945FF] to-[#14F195]">
           Mint an NFT
         </h1>
-        <h4 className="md:w-full text-center text-slate-300 my-2">
+        <h4 className="md:w-full text-center text-slate-300 my-4 text-2xl">
           <p>Upload any photo and mint it as an NFT on Solana!</p>
         </h4>
-        <div className="text-center">
+        <RequestAirdrop />
+        {wallet && <p>SOL Balance: {(balance || 0).toLocaleString()}</p>}
+        <div className="text-center mt-5">
           <input type='file' onChange={upload}/>
         </div>
         {arweave && showArweave()}
-        <button
-          className="group w-60 m-2 btn animate-pulse disabled:hidden bg-gradient-to-r from-[#9945FF] to-[#14F195] hover:from-pink-500 hover:to-yellow-500 ... "
-          onClick={mint} disabled={!nftUri || nftLink != ''}
-        >
-          <span className="block group-disabled:hidden" > 
-              Mint NFT 
-          </span>
-        </button>
+        {nftUri && mintNFT()}
         {nftLink && showNftLink()}
       </div>
     </div>
